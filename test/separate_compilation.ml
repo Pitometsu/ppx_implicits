acfeb64f381f442c9cff8b0a740f3cf7750a0ca1
jun furuse <jun.furuse@gmail.com>
2015-04-27 12:42:56 +0800

    Fixed a problem at separate compilation

--- test/separate_compilation.ml
+++ test/separate_compilation.ml
@@ -4,15 +4,15 @@
 *)
 
 open Show
-let () = assert ((Show.show ?_d:(Some Instance.int) 1) = "1")
-let () = assert ((Show.show ?_d:(Some Instance.float) 1.0) = "1.")
+
+let () = assert ((Show.show 1) = "1")
+let () = assert ((Show.show  1.0) = "1.")
 let () =
   assert
-    ((Show.show ?_d:(Some (Instance.list ~_d:Instance.int)) [1; 2; 3]) =
+    ((Show.show [1; 2; 3]) =
        "[ 1; 2; 3 ]")
 let () =
   assert
     ((Show.show
-        ?_d:(Some (Instance.list ~_d:(Instance.list ~_d:Instance.int)))
         [[1]; [2; 3]; [4; 5; 6]])
        = "[ [ 1 ]; [ 2; 3 ]; [ 4; 5; 6 ] ]")
