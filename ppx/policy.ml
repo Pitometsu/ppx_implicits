ee4a76939afa30c796e9e69985be3f22d958ed96
jun furuse <jun.furuse@gmail.com>
2015-05-19 19:05:46 +0800

    Policy DSL: Just, Opened and (,) are added

--- ppx/policy.ml
+++ ppx/policy.ml
@@ -12,18 +12,30 @@ open Ppxx
 open Longident
 
 type t = 
-  | Just of Longident.t
-(*
+  | Or of t2 list
+
+and t2 = 
+  | Opened of t3
+  | Direct of t3
+
+and t3 =
   | In of Longident.t
-  | In_rec of Longident.t
-  | Or of t list
-  | Opened of t
-*)
+  | Just of Longident.t
 
 let to_string = 
-  let open Format in 
-  function
-  | Just lid -> ksprintf (fun x -> x) "%a" Pprintast.default#longident lid
+  let open Format in
+  let rec t = function
+    | Or [] -> assert false
+    | Or [x] -> t2 x
+    | Or xs -> String.concat ", " (map t2 xs)
+  and t2 = function
+    | Direct x -> t3 x
+    | Opened x -> Printf.sprintf "Opened (%s)" (t3 x)
+  and t3 = function
+    | In lid -> ksprintf (fun x -> x) "%a" Pprintast.default#longident lid
+    | Just lid -> ksprintf (fun x -> x) "Just %a" Pprintast.default#longident lid
+  in
+  t 
 
 let prefix = "Policy_"
 
@@ -81,9 +93,31 @@ let from_string s =
       raise e
 
 let from_expression e = 
-  match e.pexp_desc with
-  | Pexp_construct ({txt=lid}, None) -> Just lid
-  | _ -> failwith "illegal policy expression"
+  let get_lid e = match e.pexp_desc with
+    | Pexp_construct ({txt=lid}, None) -> Some lid
+    | _ -> None
+  in
+  let rec t e = match e.pexp_desc with
+    | Pexp_tuple xs -> Or (map t2 xs)
+    | _ -> Or [t2 e]
+  and t2 e = match e.pexp_desc with
+    | Pexp_construct ({txt=Lident "Opened"}, Some e) -> Opened (t3 e)
+    | _ -> Direct (t3 e)
+  and t3 e = match e.pexp_desc with
+    | Pexp_construct ({txt=Lident "Just"}, Some e) ->
+        begin match get_lid e with
+        | Some lid -> Just lid
+        | None -> assert false (* error *)
+        end
+    | Pexp_construct ({txt=Lident "In"}, Some e) ->
+        begin match get_lid e with
+        | Some lid -> In lid
+        | None -> assert false (* error *)
+        end
+    | Pexp_construct ({txt=lid}, None) -> In lid
+    | _ -> failwith "illegal policy expression"
+  in
+  t e
 
 let from_structure str =
   match str with
@@ -132,53 +166,149 @@ eprintf "from_module_type: @[%a@]@." Printtyp.modtype mty;
   | [_] -> assert false (* CR jfuruse: better error handling *)
   | _ -> assert false
 
-let candidates loc env = function
-  | Just lid ->
+let check_module loc env lid =
+  match 
+    (* CR jfuruse: what is load parameter? *)  
+    try Some (Env.lookup_module ~load:true lid env) with _ -> None
+  with
+  | None ->
+      errorf "%a: no module found: %a" Location.print_loc loc Longident.format lid
+  | Some path ->
       match 
-        (* CR jfuruse: what is load parameter? *)  
-        try Some (Env.lookup_module ~load:true lid env) with _ -> None
+        try Some (Env.find_module path env) with _ -> None
       with
-      | None ->
-          errorf "%a: no module found: %a" Location.print_loc loc Longident.format lid
-      | Some path ->
-          match 
-            try Some (Env.find_module path env) with _ -> None
-          with
-          | None -> 
-              errorf "%a: no module desc found: %a" Location.print_loc loc Path.format path
-          | Some mdecl -> 
-              let rec get_candidates env lid mty =
-                let sg = 
-                  try
-                    match Env.scrape_alias env @@ Mtype.scrape env mty with
-                    | Mty_signature sg -> sg
-                    | _ -> assert false
-                  with
-                  | e -> 
-                      eprintf "scraping failed: %s" & Printexc.to_string e;
-                      raise e
-                in
-                flip2 fold_right sg [] & fun sitem st -> match sitem with
-                | Sig_value (id, _vdesc) ->
-                   (* CR jfuruse: 
-                      I don't undrestand yet why the above _vdesc is not appropriate
-                      and we must get vdesc like below.
-                   *)
-                    let lid = Ldot (lid, Ident.name id) in
-                    begin try
-                      let path, vdesc = Env.lookup_value lid env in
-                      (lid, path, vdesc) :: st
-                    with
-                    | Not_found ->
-                        warn (fun () -> 
-                          eprintf "%%imp instance %a is not accessible in the current scope therefore ignored." Longident.format lid);
-                        st
-                    end
+      | None -> 
+          errorf "%a: no module desc found: %a" Location.print_loc loc Path.format path
+      | Some mdecl -> mdecl
 
-                | Sig_module (id, moddecl, _) -> 
-                    let lid = Ldot (lid, Ident.name id) in
-                    get_candidates env lid moddecl.Types.md_type @ st
+let check_value env lid =
+  try
+    let path, vdesc = Env.lookup_value lid env in
+    Some (path, vdesc)
+  with
+  | Not_found ->
+      warn (fun () -> 
+        eprintf "%%imp instance %a is not accessible in the current scope therefore ignored." Longident.format lid);
+      None
+  
+let scrape_sg env mdecl = 
+  try
+    match Env.scrape_alias env & Mtype.scrape env mdecl.md_type with
+    | Mty_signature sg -> sg
+    | _ -> assert false
+  with
+  | e -> 
+      eprintf "scraping failed: %s" & Printexc.to_string e;
+      raise e
 
-                | _ -> st
-              in
-              get_candidates env lid mdecl.md_type
+let rec get_candidates ~recursive env lid mdecl =
+  let sg = scrape_sg env mdecl in
+  flip2 fold_right sg [] & fun sitem st -> match sitem with
+  | Sig_value (id, _vdesc) ->
+      (* CR jfuruse: 
+         I don't undrestand yet why the above _vdesc is not appropriate
+         and we must get vdesc like below.
+      *)
+      let lid = Ldot (lid, Ident.name id) in
+      begin match check_value env lid with
+      | None -> st
+      | Some (path, vdesc) -> (lid, path, vdesc) :: st
+      end
+
+  | Sig_module (id, moddecl, _) when recursive -> 
+      let lid = Ldot (lid, Ident.name id) in
+      get_candidates ~recursive env lid moddecl @ st
+        
+  | _ -> st
+
+let rec get_opens = function
+  | Env.Env_empty -> []
+  | Env_value (s, _, _)
+  | Env_type (s, _, _)
+  | Env_extension (s, _, _)
+  | Env_module (s, _, _)
+  | Env_modtype (s, _, _)
+  | Env_class (s, _, _)
+  | Env_cltype (s, _, _)
+  | Env_functor_arg (s, _) -> get_opens s
+  | Env_open (s, path) -> path :: get_opens s
+
+let get_opens env = get_opens & Env.summary env
+
+let module_lids_in_open_path env lids = function
+  | None -> 
+      flip filter_map lids (fun lid ->
+        try
+          Some (Env.lookup_module ~load:false (*?*) lid env)
+        with
+        | _ -> None)
+  | Some (Path.Pident id) when Ident.name id = "Pervasives" && Ident.persistent id  -> 
+      (* We assume Pervasives has no instances *)
+      []
+  | Some open_ ->
+      (*  eprintf "open %a@." Path.format open_; *)
+      let mdecl = Env.find_module open_ env in (* It should succeed *)
+      let sg = scrape_sg env mdecl in
+      let env = Env.open_signature Asttypes.Fresh open_ sg env in
+      flip filter_map lids (fun lid ->
+        try
+          Some (Env.lookup_module ~load:false (*?*) lid env)
+        with
+        | _ -> None)
+      
+let module_lids_in_open_paths env lids opens =
+  concat_map (module_lids_in_open_path env lids) opens
+
+let candidates loc env = 
+  let rec t = function
+    | Or ts ->  concat & map t2 ts
+  and t2 = function
+    | Opened x -> 
+        let lid = match x with
+          | Just lid -> lid
+          | In lid -> lid
+        in
+        let make lid = match x with
+          | Just _ -> Direct (Just lid)
+          | In _ -> Direct (In lid)
+        in
+        let opens = get_opens env in
+        let paths = 
+          concat 
+          & map (module_lids_in_open_path env [lid]) 
+          & None :: map (fun x -> Some x) opens
+        in
+        (* We translate paths to longidents.
+           for the instance name spacing by 'open', it seems ok.
+
+           module X = struct
+             module Show = struct
+             end
+           end
+           module Y = struct
+             module Show = struct
+             end
+           end
+           open X
+           open Y
+
+           To access values inside X.Show, Show is not good since
+           it is shadowed by open Y. 
+        *)
+        (* CR jfuruse: need to check lids are really point paths *)
+        let lids = map Untypeast.lident_of_path paths in
+        t & Or (map make lids)
+    | Direct x -> t3 x
+  and t3 = function
+    | Just lid ->
+        let mdecl = check_module loc env lid in
+        get_candidates ~recursive:false env lid mdecl
+    | In lid ->
+        let mdecl = check_module loc env lid in
+        get_candidates ~recursive:true env lid mdecl
+  in
+  t
+
+let candidates loc env t =
+  sort_uniq (fun (l1,p1,_) (l2,p2,_) -> compare (l1,p1) (l2,p2)) 
+  & candidates loc env t
