b8acc89b068daa5383c27e5dc85e5418fd2a0772
jun furuse <jun.furuse@gmail.com>
2015-05-19 15:29:33 +0800

    Typing in PPX produced gaga.cmi by mistake

--- ppx/ppx.ml
+++ ppx/ppx.ml
@@ -10,10 +10,12 @@ let preconvert_structure = preconvert.structure preconvert
 let mapper = { 
   default_mapper with
     structure = (fun x str ->
+    Clflags.dont_write_files := true;
     match Ast_mapper.tool_name () with
     | "ocamldep" -> default_mapper.structure x str
     | _ -> Compile.implementation Format.err_formatter "papa" (preconvert_structure str) "gaga");
   signature = (fun x sg -> 
+    Clflags.dont_write_files := true;
     match Ast_mapper.tool_name () with
     | "ocamldep" -> default_mapper.signature x sg
     | _ -> Compile.interface Format.err_formatter "papa" sg "gaga"); 
