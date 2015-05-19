ccb044fdba35adec2f431cfc9837bd8d36b16aea
jun furuse <jun.furuse@gmail.com>
2015-05-19 16:19:00 +0800

    Warnings are surpressed

--- ppx/ppx.ml
+++ ppx/ppx.ml
@@ -12,11 +12,15 @@ let mapper = {
   default_mapper with
     structure = (fun x str ->
     Clflags.dont_write_files := true;
+    Warnings.parse_options false "a";
+    Warnings.parse_options true "a";
     match Ast_mapper.tool_name () with
     | "ocamldep" -> default_mapper.structure x str
     | _ -> Compile.implementation Format.err_formatter "papa" (preconvert_structure str) "gaga");
   signature = (fun x sg -> 
     Clflags.dont_write_files := true;
+    Warnings.parse_options false "a";
+    Warnings.parse_options true "a";
     match Ast_mapper.tool_name () with
     | "ocamldep" -> default_mapper.signature x sg
     | _ -> Compile.interface Format.err_formatter "papa" (preconvert_signature sg) "gaga"); 
