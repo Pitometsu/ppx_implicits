f850952ea5a63ca2e9e8af435bb1bfd57e66375e
Jun Furuse <jun.furuse@gmail.com>
2015-06-06 12:22:50 +0900

    ppx_implicits now understands compiler options

--- ppx/ppxx.ml
+++ ppx/ppxx.ml
@@ -151,44 +151,172 @@ end
 
 let ppx_name = ref "ppx name is not set"
 
-let test mapper fname = 
-  try
-    if Filename.check_suffix fname ".ml" then (* .mlt ? *)
-      let str = Pparse.parse_implementation ~tool_name:"ppx" Format.err_formatter fname in
-      let str = mapper.structure mapper str in
-      Pprintast.structure Format.std_formatter str
-    else if Filename.check_suffix fname ".mli" then 
-      let sg = Pparse.parse_interface ~tool_name:"ppx" Format.err_formatter fname in
-      let sg = mapper.signature mapper sg in
-      Pprintast.signature Format.std_formatter sg
-    else assert false;
-    Format.fprintf Format.std_formatter "@."
-  with
+let handle_error f =
+  try f () with 
   | Syntaxerr.Error e ->
       !!% "%a@." Syntaxerr.report_error e;
       exit 2
   | e ->
       Format.eprintf "%a@."  Location.report_exception e;
       exit 2
+  
+    
+let impl mapper fname =
+  handle_error & fun () ->
+    let str = Pparse.parse_implementation ~tool_name:"ppx" Format.err_formatter fname in
+    let str = mapper.structure mapper str in
+    Pprintast.structure Format.std_formatter str;
+    Format.fprintf Format.std_formatter "@."
+
+let intf mapper fname =
+  handle_error & fun () ->
+    let sg = Pparse.parse_interface ~tool_name:"ppx" Format.err_formatter fname in
+    let sg = mapper.signature mapper sg in
+    Pprintast.signature Format.std_formatter sg;
+    Format.fprintf Format.std_formatter "@."
+    
+let anonymous mapper fname = 
+  if Filename.check_suffix fname ".ml" then impl mapper fname (* .mlt ? *)
+  else if Filename.check_suffix fname ".mli" then intf mapper fname 
+  else assert false
 
 let debug_resolve = ref false
 let debug_unif = ref false
 
 let run name mapper = 
   ppx_name := name;
+
+  let module Options = Main_args.Make_bytecomp_options (struct
+open Clflags
+open Compenv
+
+let show_config () =
+  Config.print_config stdout;
+  exit 0;
+;;
+  let set r () = r := true
+  let unset r () = r := false
+  let _a = set make_archive
+  let _absname = set Location.absname
+  let _annot = set annotations
+  let _binannot = set binary_annotations
+  let _c = set compile_only
+  let _cc s = c_compiler := Some s
+  let _cclib s = ccobjs := Misc.rev_split_words s @ !ccobjs
+  let _ccopt s = first_ccopts := s :: !first_ccopts
+  let _compat_32 = set bytecode_compatible_32
+  let _config = show_config
+  let _custom = set custom_runtime
+  let _dllib s = dllibs := Misc.rev_split_words s @ !dllibs
+  let _dllpath s = dllpaths := !dllpaths @ [s]
+  let _for_pack s = for_package := Some s
+  let _g = set debug
+  let _i () = print_types := true; compile_only := true
+  let _I s = include_dirs := s :: !include_dirs
+  let _impl = impl mapper
+  let _intf = intf mapper
+  let _intf_suffix s = Config.interface_suffix := s
+  let _keep_locs = set keep_locs
+  let _labels = unset classic
+  let _linkall = set link_everything
+  let _make_runtime () =
+    custom_runtime := true; make_runtime := true; link_everything := true
+  let _no_alias_deps = set transparent_modules
+  let _no_app_funct = unset applicative_functors
+  let _noassert = set noassert
+  let _nolabels = set classic
+  let _noautolink = set no_auto_link
+  let _nostdlib = set no_std_include
+  let _o s = output_name := Some s
+  let _open s = open_modules := s :: !open_modules
+  let _output_obj () = output_c_object := true; custom_runtime := true
+  let _pack = set make_package
+  let _pp s = preprocessor := Some s
+  let _ppx s = first_ppx := s :: !first_ppx
+  let _principal = set principal
+  let _rectypes = set recursive_types
+  let _runtime_variant s = runtime_variant := s
+  let _safe_string = unset unsafe_string
+  let _short_paths = unset real_paths
+  let _strict_sequence = set strict_sequence
+  let _strict_formats = set strict_formats
+  let _thread = set use_threads
+  let _vmthread = set use_vmthreads
+  let _unsafe = set fast
+  let _unsafe_string = set unsafe_string
+  let _use_prims s = use_prims := s
+  let _use_runtime s = use_runtime := s
+  let _v () = print_version_and_library "compiler"
+  let _version = print_version_string
+  let _vnum = print_version_string
+  let _w = (Warnings.parse_options false)
+  let _warn_error = (Warnings.parse_options true)
+  let _warn_help = Warnings.help_warnings
+  let _where = print_standard_library
+  let _verbose = set verbose
+  let _nopervasives = set nopervasives
+  let _dsource = set dump_source
+  let _dparsetree = set dump_parsetree
+  let _dtypedtree = set dump_typedtree
+  let _drawlambda = set dump_rawlambda
+  let _dlambda = set dump_lambda
+  let _dinstr = set dump_instr
+  let anonymous = anonymous mapper
+end)
+  in
+  let inappropriate =
+    [ "-a"
+    ; "-c"
+    ; "-cc"
+    ; "-cclib"
+    ; "-ccopt"
+    ; "-compat-32"
+    ; "-custom"
+    ; "-dllib"
+    ; "-dllpath"
+    ; "-for-pack"
+    ; "-g"
+    (* ; "-i" *)
+    ; "-linkall"
+    ; "-make-runtime"
+    ; "-make_runtime"
+    ; "-noassert"
+    ; "-noautolink"
+    ; "-o"
+    ; "-output-obj"
+    ; "-pack"
+    ; "-pp"
+    ; "-ppx"
+    ; "-runtime-variant"
+    ; "-use-runtime"
+    ; "-use_runtime"
+    ; "-where"
+    ; "-"
+    ; "-nopervasives"
+    ; "-use-prims"
+    ; "-drawlambda"
+    ; "-dlambda"
+    ; "-dinstr"
+    ]
+  in
+    
+  let option_list = List.filter (fun (n,_,_) ->
+    not & List.mem n inappropriate) Options.list
+  in
+  
   let debug = ref false in
   let rev_files = ref [] in 
   Arg.parse 
-    [ "-debug", Arg.Set debug, "debug mode which can take .ml/.mli then print the result"
-    ; "-debug-resolve", Arg.Set debug_resolve, "debug mode to print overload resolution"
-    ; "-debug-unif", Arg.Set debug_unif, "debug mode to print unification results"
-    ]
+    ([ "-debug", Arg.Set debug, "debug mode which can take .ml/.mli then print the result"
+     ; "-debug-resolve", Arg.Set debug_resolve, "debug mode to print overload resolution"
+     ; "-debug-unif", Arg.Set debug_unif, "debug mode to print unification results"
+     ] @ option_list)
     (fun s -> rev_files := s :: !rev_files) (* will be handled by [Ast_mapper.apply] *)
     name;
   try
     match !debug, List.rev !rev_files with
     | true, files ->
-        List.iter (test mapper) files
+        List.iter (anonymous mapper) files
     | false, [infile; outfile] ->
         Ast_mapper.apply ~source:infile ~target:outfile mapper
     | _ -> 
--- tests/template.ml
+++ tests/template.ml
@@ -2,25 +2,8 @@
   Try to share the same code using functor.
 *)
 
-module Make(A : sig
-  type 'a t
-  (* type __imp_policy__ *)
-end) = struct
-  [@@@warning "-16"]
-  type 'a __imp__ = Packed of 'a A.t
-  (* Zuut, we cannot copy the policy! *)
-  (* type __imp_policy__ = A.__imp_policy__ *) 
-  module Instance = struct
-    let pack ~_x = Packed _x
-    let pack_opt ~_x = Some (Packed _x)
-  end
-  let unpack_opt ?_imp = match _imp with
-    | None -> assert false
-    | Some (Packed x) -> x
-end
-
 module Show = struct
-  include Make(struct  type 'a t = 'a -> string end)
+  include Ppx_implicits.Implicits.Make(struct  type 'a t = 'a -> string end)
   [%%imp_policy Show.Instance, opened ShowInstance]
 end
 
