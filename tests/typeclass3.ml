568e498e3408df97dd47cfae4abbf388cec379c4
jun furuse <jun.furuse@gmail.com>
2015-06-29 18:26:06 +0800

    added tests/typeclass3.ml

---
+++ tests/typeclass3.ml
@@ -0,0 +1,37 @@
+module type Show = sig
+  type a
+  val show : a -> string
+end
+
+(* The above with [%typeclass] should produce the following *)
+
+module Show = struct
+
+  type 'a s = (module Show with type a = 'a)
+
+  include Ppx_implicits.Implicits.Make(struct
+    type 'a t = 'a s
+  end)
+
+  [%%imp_policy opened ShowInstance]
+
+  let show (type a) ?_imp = let module M = (val (unpack_opt ?_imp : a s)) in M.show
+end
+
+let show = Show.show
+
+module Int = struct
+  module ShowInt = struct
+    type a = int
+    let show  = string_of_int
+  end
+
+  (* The above with [%implicit Show] should produce the following *)
+ 
+  module ShowInstance = struct
+    let int : int Show.s = (module ShowInt)
+  end
+end
+
+open Int
+let () = assert (show 1 = "1")
