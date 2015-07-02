14eccdf949d3390e4b296bae82732eb4a6f38b52
jun furuse <jun.furuse@gmail.com>
2015-07-02 20:38:41 +0800

    extended the example

--- tests/typeclass4.ml
+++ tests/typeclass4.ml
@@ -6,11 +6,21 @@ end [@@typeclass]
 let show = Show.show
 
 module Int = struct
-  module ShowInt = struct
+  module M = struct
     type a = int
     let show  = string_of_int
   end [@@instance Show]
 end
 
+module Float = struct
+  module M = struct
+    type a = float
+    let show  = string_of_float
+  end [@@instance Show]
+end
+
 open Int
+open Float
+
 let () = assert (show 1 = "1")
+let () = assert (show 1.2 = "1.2")
