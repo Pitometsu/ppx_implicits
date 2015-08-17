5bc0d57e5e214972e837769b02950215afa8995b
jun furuse <jun.furuse@gmail.com>
2015-08-17 19:30:05 +0800

    added an example of instances at the toplevel scope

---
+++ tests/nomodule.ml
@@ -0,0 +1,25 @@
+module type Coerce = sig
+  type a
+  type b
+  val coerce : a -> b
+end [@@typeclass]
+
+(* Instance defined not within a submodule *)
+  
+module IF = struct
+  type a = int
+  type b = float
+  let coerce = float_of_int
+end [@@instance Coerce]
+
+module FI = struct
+  type a = float
+  type b = int
+  let coerce = int_of_float
+end [@@instance Coerce]
+
+module N : sig end = struct
+  let x = ref None (* '_a option ref *)
+
+  let () = x := Some (Coerce.coerce 1) (* should instantiate '_a to float *)
+end
