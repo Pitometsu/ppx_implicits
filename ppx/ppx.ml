296745d19a91c63341dee4be36c280af95aebe31
jun furuse <jun.furuse@gmail.com>
2015-12-22 13:56:36 +0800

    added -debug-resolve and -debug-unif. PPX_IMPLICITS_DEBUG_RESOLVE and PPX_IMPLICITS_DEBUG_UNIF env vars are also usable

--- ppx/ppx.ml
+++ ppx/ppx.ml
@@ -4,7 +4,9 @@ open Ast_mapper
 
 module M = Typpx.Make.F(struct
   let tool_name = "ppx_implicit"
-  let args = []
+  let args = [ "-debug-resolve", Arg.Set Options.debug_resolve, "Ppx_implicits: resolution debug"
+             ; "-debug-unif", Arg.Set Options.debug_unif, "Ppx_implicits: unification debug"
+             ]
   let firstUntypedTransformation = Pre.extend default_mapper
   module Typemod = Typpx.Default.Typemod
   module TypedTransformation = Mod.Map
