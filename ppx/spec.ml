f1fc13a66fb5aaceb8a5c8fc88ec74297dd11a6b
jun furuse <jun.furuse@gmail.com>
2016-01-04 15:59:03 +0800

    merge

--- ppx/spec.ml
+++ ppx/spec.ml
@@ -33,16 +33,18 @@ end
 
 (** spec dsl *)
 type t = 
-  | Or of t2 list
+  | Or of t2 list (** [t2, .., t2] *)
   | Type (** [%imp].  Encoded as a type definition.  No allowed in [%%imp_spec] *)
 
 and t2 = 
-  | Opened of Longident.t flagged
+  | Opened of Longident.t flagged (** [opened M]. The values defined under module path [P.M] which is accessible as [M] by [open P] *)
   | Direct of (Longident.t * Path.t option) flagged
-  | Aggressive of t2
-  | Related
-  | Name of string * Re.re * t2
-  | Typeclass of Path.t option (* None at parsing, but must be filled with Some until the resolution *)
+    (** [P] or [just P]. [P] is for the values defined under module [P] and [P]'s sub-modules. [just P] is for values defined just under module [P] and values defined in its sub-modules are not considered. *)
+  | Aggressive of t2 (** [aggressive t2]. Even normal function arrows are considered as constraints. *)
+  | Related (** [related]. The values defined under module [P] where data type defined in [P] appears in the type of the resolution target *)
+  | Name of string * Re.re * t2 (** [name "rex" t2]. Constraint values only to those whose names match with the regular expression *)
+  | Typeclass of Path.t option (** [typeclass]. Typeclass style resolution. The argument is None at parsing, but must be filled with Some until the resolution.
+  None at parsing, but must be filled with Some until the resolution *) 
   | Deriving of Longident.t (** [deriving M]. [M] must define [M.tuple], [M.object_] and [M.poly_variant] *)
 
 and 'a flagged = In of 'a | Just of 'a
