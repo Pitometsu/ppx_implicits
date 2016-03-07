fb57f39adb2f0e778af409d8ac4dcacd643f7dfb
jun furuse <jun.furuse@gmail.com>
2016-03-07 17:38:43 +0800

    test fix

--- tests/imp3.ml
+++ tests/imp3.ml
@@ -31,5 +31,15 @@ let () = assert (show_twice [1;2] = "[ 1; 2 ][ 1; 2 ]")
 (* Forgetting to specify the relation with _d and x *)
 let show_twice ?_d:(_ : ('a -> string, [%imp_spec Show]) Ppx_implicits.Runtime.t option) x = show x ^ show x
 
-(* This inf loops! *)
+(* This is now rejected correctly:
+
+Error: File "imp3.ml", line 35, characters 17-27:
+  The experssion has type 'a -> string which is too ambiguous to resolve this implicit.
+  The following instances may cause infinite loop of the resolution:
+    Show.list
+
+We should print the strange type of show_twice to help the understanding of the error.
+
 let () = assert (show_twice 1 = "11")       (* ?_d should have a free type variable there it must fail *)
+*)
+  
