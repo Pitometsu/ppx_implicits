89b6c2d880db11309928386ebb75f66adda11f2b
Jun Furuse <jun.furuse@gmail.com>
2016-03-16 02:19:06 +0800

    added tests/add.ml

---
+++ tests/add.ml
@@ -0,0 +1,11 @@
+module Add = struct
+  let int = (+)
+  let float = (+.)
+end
+
+type 'a add = ('a -> 'a -> 'a, [%imp_spec Add]) Ppx_implicits.t
+
+let add : ?_d:'a add -> 'a -> 'a -> 'a = Ppx_implicits.imp
+
+let () = assert (add 1 2 = 3)
+let () = assert (add 1.2 3.4 = 4.6)
