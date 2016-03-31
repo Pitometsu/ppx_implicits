a55d41930a28fc5e237cd33ac99669dfda6ecf3d
jun furuse <jun.furuse@gmail.com>
2016-03-31 13:15:30 +0800

    example cleanups

--- old_tests/kgap.ml
+++
@@ -1,8 +0,0 @@
-module Show = struct
-  let int = string_of_int
-  let float = string_of_float
-  let list ~_d:show xs = "[ " ^ String.concat "; " (List.map show xs) ^ " ]"
-  let tuple ~_a:show_a (a, b) ~_b:show_b = "( " ^ show_a a ^ ", " ^ show_b b ^ " )"
-end
-
-let () =assert ([%imp Show] ([1;2],3) = "( [ 1; 2 ], 3 )")
--- old_tests/numuse.ml
+++
@@ -1,5 +0,0 @@
-open Numdef
-
-let () =
-  assert ((+) 2 2 = 4);
-  assert ((+) 1.2 1.2 = 2.4)
