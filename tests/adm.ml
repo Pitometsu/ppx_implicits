c8adb960e7c60296581525118b09e9523652d9b2
Jun Furuse <jun.furuse@gmail.com>
2017-07-25 12:33:34 +0900

    fixed tests

--- tests/adm.ml
+++ tests/adm.ml
@@ -1,16 +1,12 @@
 (* The following strange recipes cannot be composed recursively, 
    since their recusrive use do not strictly decrease the type size.
-
-   Try
-
-   $ ../ppx/ppx_implicits.exe -debug -debug-resolve adm.ml
 *)
-type 'a show = ('a -> string, [%imp_spec Show]) Ppx_implicits.t
-let show : ?imp:'a show -> 'a -> string = Ppx_implicits.imp
+type 'a show = ('a -> string, [%imp Show]) Ppx_implicits.t
+let show : ?d:'a show -> 'a -> string = Ppx_implicits.imp
 
 module Show = struct
   let int = string_of_int
-  let id : ?imp:'a show -> 'a -> string = Ppx_implicits.imp
+  let id : ?d:'a show -> 'a -> string = Ppx_implicits.imp
 end
 
 let () = print_string @@ show 1
