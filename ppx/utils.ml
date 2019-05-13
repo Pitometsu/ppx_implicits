e320198db8cf629759b966209f89255521931472
Yuriy Pitomets <pitometsu@gmail.com>
2019-05-13 13:35:49 +0300

    Upgrade to ocaml 4.06

--- ppx/utils.ml
+++ ppx/utils.ml
@@ -1,4 +1,4 @@
-open Ast_405
+open Ast_406
 
 open Ppxx.Utils
 open List
@@ -16,14 +16,14 @@ module String = struct
 
   let is_prefix ?(from=0) sub str =
     let sublen = String.length sub in
-    try 
+    try
       if String.sub str from sublen = sub then Some (drop (from + sublen) str)
       else None
     with _ -> None
-end 
+end
 
-(* CR jfuruse: _path is not used *)      
-let scrape_sg _path env mdecl = 
+(* CR jfuruse: _path is not used *)
+let scrape_sg _path env mdecl =
   try
     match Env.scrape_alias env & Mtype.scrape env mdecl.md_type with
     | Mty_signature sg ->
@@ -34,7 +34,7 @@ let scrape_sg _path env mdecl =
     | Mty_functor _ -> [] (* We do not scan the internals of functors *)
     | _ -> assert false
   with
-  | e -> 
+  | e ->
       !!% "scraping failed: %s" & Printexc.to_string e;
       raise e
 
@@ -42,7 +42,7 @@ let _test_scrape_sg path env sg =
   match path with
   | Pident {Ident.name = "Pervasives"} -> ()
   | _ ->
-  let lid = (* Typpx.*) Untypeast.lident_of_path path in      
+  let lid = (* Typpx.*) Untypeast.lident_of_path path in
   !!% "SCRAPE SG %a@." Path.format_verbose path;
   flip iter sg & function
     | Sig_value (id, _vdesc) ->
@@ -51,16 +51,16 @@ let _test_scrape_sg path env sg =
         !!% "  value %a  >> %a@." Ident.format_verbose id (Option.format Path.format_verbose) popt
     | Sig_module (id, _moddecl, _) ->
         !!% "  module %a@." Ident.format_verbose id
-    | Sig_type (id, _, _) -> 
+    | Sig_type (id, _, _) ->
         !!% "  type %a@." Ident.format_verbose id
     | _ -> ()
-    
+
 (** Build an empty type env except mp module with the given module type *)
 class dummy_module env mp mty =
   (* Env.lookup_* does not support Mty_alias (and probably Mty_indent) *)
   let mty = Env.scrape_alias env & Mtype.scrape env mty in
 (*
-  let () = !!% "dummy_module of @[%a@]@." Printtyp.modtype mty in 
+  let () = !!% "dummy_module of @[%a@]@." Printtyp.modtype mty in
 *)
   let dummy = "Dummy" in
   let id = Ident.create "Dummy" in
@@ -78,12 +78,12 @@ object
     match Env.lookup_module ~load:false (Longident.(Ldot (Lident dummy, s))) env with
     | Pdot (_, s', n) -> Pdot (mp, s', n)
     | _ -> assert false (* impos *)
-    
+
   method lookup_value s =
     match Env.lookup_value (Longident.(Ldot (Lident dummy, s))) env with
     | Pdot (_, s', n), _vd -> Pdot (mp, s', n)
     | _ -> assert false (* impos *)
-    
+
 end
 
 let exit_then d f = try f () with Exit -> d
@@ -118,10 +118,10 @@ module Option = struct
 end
 
 let from_Some = Option.from_Some
-  
+
 module List = struct
   include Ppxx.Utils.List
-    
+
   (* Haskell's splitAt. Borrowed from Spotlib. Tested. *)
   let split_at n xs =
     let rec split_at_ n st xs =
@@ -134,7 +134,7 @@ module List = struct
     rev r, dropped
 end
 
-let mangle s = 
+let mangle s =
   let len = String.length s in
   let b = Buffer.create len in
   for i = 0 to len - 1 do
@@ -142,24 +142,24 @@ let mangle s =
     match c with
     | 'A'..'Z' | 'a'..'z' | '0'..'9' | '\'' -> Buffer.add_char b c
     | '_' -> Buffer.add_string b "__"
-    | _ -> 
+    | _ ->
         Buffer.add_char b '_';
         Buffer.add_string b & Printf.sprintf "%02x" & Char.code c
   done;
   Buffer.contents b
 
 (* CR jfuruse: need tests *)
-let unmangle s = 
+let unmangle s =
   try
     let len = String.length s in
     let b = Buffer.create len in
-    let rec f i = 
+    let rec f i =
       if i = len then ()
       else begin
         let c = String.unsafe_get s i in
         match c with
         | 'A'..'Z' | 'a'..'z' | '0'..'9' | '\'' -> Buffer.add_char b c; f & i+1
-        | '_' -> 
+        | '_' ->
             begin match s.[i+1] with
             | '_' -> Buffer.add_char b '_'; f & i+2
             | _ ->
@@ -176,7 +176,7 @@ let unmangle s =
   with
   | Failure e -> Error (`Failed_unmangle (s ^ ": " ^ e))
 
-let expression_from_string s = 
+let expression_from_string s =
   let lexbuf = Lexing.from_string s in
   try Ok (Parser.parse_expression Lexer.token lexbuf) with
   | _ -> Error (`Parse s)
@@ -206,7 +206,7 @@ let sig_module_of_stri sitem =
   | Pstr_modtype mtd ->
       Ppxx.Helper.Sig.module_ ~loc:sitem.pstr_loc
         { pmd_name       = mtd.pmtd_name
-        ; pmd_type       = from_Some mtd.pmtd_type
+        ; pmd_type       = from_Some (mtd.pmtd_type : Ast_405.Parsetree.module_type option)
         ; pmd_attributes = mtd.pmtd_attributes
         ; pmd_loc        = mtd.pmtd_loc
         }
@@ -224,17 +224,17 @@ let rec values_of_module ~recursive env path mdecl : Path.t list =
           assert false
         in
         path :: st
-    | Sig_module (id, moddecl, _) when recursive -> 
+    | Sig_module (id, moddecl, _) when recursive ->
         let path = m#lookup_module & Ident.name id in
         values_of_module ~recursive env path moddecl @ st
-          
+
     | _ -> st
 
 let check_module env loc path =
-  match 
+  match
     try Some (Env.find_module path env) with _ -> None
   with
-  | None -> 
+  | None ->
       raise_errorf "%a: no module desc found: %a" Location.format loc Path.format path
   | Some mdecl -> mdecl
 
@@ -248,7 +248,7 @@ let format_expression ppf e =
   & (* Typpx. *) Untypeast.(default_mapper.expr default_mapper) e
 
 let is_none e = match e.Typedtree.exp_desc with
-  | Texp_construct ({Location.txt=Lident "None"}, _, []) -> 
+  | Texp_construct ({Location.txt=Lident "None"}, _, []) ->
       begin match is_option_type e.exp_env e.exp_type with
       | None ->
           !!% "is_none: the input is type-corrupted@."; assert false
